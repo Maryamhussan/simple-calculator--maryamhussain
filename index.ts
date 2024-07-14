df074f69fbda120fb2d1b4b5dd9bb2fcc6d91051
Maryam Hussain <maryamghayyas2005@gmail.com>
2024-07-15 00:23:59 +0500

    ini the repo

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "Firstnumber" },
+  { message: "Enter Second Number", type: "number", name: "secondnumber" },
+  {
+    message: "Select One Of the operators to perform Operations",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "substraction", "Multiplication", "Division"],
+  },
+]);
+//conditional instatement
+if (answer.operator === "addition") {
+  console.log(answer.Firstnumber + answer.secondnumber);
+} else if (answer.operator === "substraction") {
+  console.log(answer.Firstnumber - answer.secondnumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.Firstnumber * answer.secondnumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.Firstnumber / answer.secondnumber);
+} else {
+  console.log("please select operator");
+}
